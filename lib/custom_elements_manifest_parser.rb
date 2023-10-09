9046148201b312b7be18b1da7b9f1e9c0f9a484b
Konnor Rogers <konnor5456@gmail.com>
2023-10-08 23:35:45 -0400

    add a yardoc

--- lib/custom_elements_manifest_parser.rb
+++ lib/custom_elements_manifest_parser.rb
@@ -6,6 +6,7 @@ module CustomElementsManifestParser
   Dir["#{__dir__}/custom_elements_manifest_parser/**/*.rb"].each { |file| require file }
 
   # Shortuct for `CustomElementsManifestParser::Parser.new().parse()`
+  # @return [CustomElementsManifestParser::Parser]
   def self.parse(hash)
     Parser.new(hash).parse
   end
